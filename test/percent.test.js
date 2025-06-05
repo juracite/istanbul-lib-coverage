14fb38f4067a0b4cf3b8a5ee95a16c71d1d05db7
YLM <yannlm2010@gmail.com>
2025-06-05 16:45:10 +0200

    Add test case for zero total percent

--- test/percent.test.js
+++ test/percent.test.js
@@ -15,4 +15,8 @@ describe('percent calculation', () => {
 
         assert.isBelow(p, 100);
     });
+    it('returns 100 when total is 0', () => {
+        const p = percent(5, 0);
+        assert.equal(p, 100);
+    });
 });
